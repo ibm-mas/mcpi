8b94ce669fb81dd8318e3c58b8a486fd36ef15d6
Brian Zhu <bzhu@ca.ibm.com>
2025-04-17 15:13:35 -0400

    [patch] mas-db-exporter

--- docs/download/mas-db-exporter/enable-mas-db-exporter.sh
+++ docs/download/mas-db-exporter/enable-mas-db-exporter.sh
@@ -16,14 +16,20 @@ get_mas_instance_name() {
 # retreive mas instance name
 MAS_MANAGE_NS=$1
 
+if [ ! -d /opt/app-root/src/db-exporter ]; then 
+  mkdir /opt/app-root/src/db-exporter
+fi
+
 # configure mas-db-exporter
 log_info "configure mas-db-exporter"
-wget -O mas-db-exporter.sh https://ibm-mas.github.io/mcpi/download/mas-db-exporter/mas-db-exporter.sh >/dev/null 2>&1
-wget -O mas-db-exporter.sample https://ibm-mas.github.io/mcpi/download/mas-db-exporter/mas-db-exporter.sample >/dev/null 2>&1
-sed "s/MAS_MANAGE_NS=\"mas-masinst1-manage\"/MAS_MANAGE_NS=\"$MAS_MANAGE_NS\"/g" ./mascore-stats.sample > /opt/app-root/src/server/metricscript/${MAS_MANAGE_NS}-mas-db-exporter
+wget -O /opt/app-root/src/db-exporter/mas-db-exporter.sh https://ibm-mas.github.io/mcpi/download/mas-db-exporter/mas-db-exporter.sh >/dev/null 2>&1
+wget -O /opt/app-root/src/db-exporter/mas-db-exporter.sample https://ibm-mas.github.io/mcpi/download/mas-db-exporter/mas-db-exporter.sample >/dev/null 2>&1
+sed "s/MAS_MANAGE_NS=\"mas-masinst1-manage\"/MAS_MANAGE_NS=\"$MAS_MANAGE_NS\"/g" /opt/app-root/src/db-exporter/mas-db-exporter.sample > /opt/app-root/src/server/metricscript/${MAS_MANAGE_NS}-db-exporter
 
 # configure mas-db-expoert-podmonitor
-wget -O mas-db-exporter-podmonitor.yaml https://ibm-mas.github.io/mcpi/download/mas-db-exporter/mas-db-exporter-podmonitor.yaml >/dev/null 2>&1
-sed "s/mas-manage-db-exporter/${MAS_MANAGE_NS}-db-exporter/g" ./mas-db-exporter-podmonitor.yaml >${MAS_MANAGE_NS}-mas-db-exporter-podmonitor.yaml
-oc apply -f ./${MAS_MANAGE_NS}-mas-db-exporter-podmonitor.yaml  >/dev/null 2>&1
+wget -O /opt/app-root/src/db-exporter/mas-db-exporter-podmonitor.yaml https://ibm-mas.github.io/mcpi/download/mas-db-exporter/mas-db-exporter-podmonitor.yaml >/dev/null 2>&1
+sed "s/mas-manage-db-exporter/${MAS_MANAGE_NS}-db-exporter/g" /opt/app-root/src/db-exporter/mas-db-exporter-podmonitor.yaml >${MAS_MANAGE_NS}-mas-db-exporter-podmonitor.yaml
+oc apply -f /opt/app-root/src/db-exporter/${MAS_MANAGE_NS}-mas-db-exporter-podmonitor.yaml  >/dev/null 2>&1
 
+# change the permission
+chmod 700 /opt/app-root/src/server/metricscript/${MAS_MANAGE_NS}-db-exporter
